83de8b3c6ecceb625f8653b4a5b1b3d22149939d
Brask8 <antoine-34150@live.fr>
2023-04-10 21:25:04 +0200

    Displaying the window

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import numpy 
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
